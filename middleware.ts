6295e44bb0e263fa98bfd8019ba7cd7ecd58374f
Shanoor-Abdul <abdulshanoorcoder@gmail.com>
2026-06-29 21:35:55 +0300

    changed middleware.ts for auth issue

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,5 @@
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
 
-export function middleware(req: NextRequest) {
+export function middleware() {
   return NextResponse.next();
 }
-
-export const config = {
-  matcher: ["/admin/:path*"],
-};
